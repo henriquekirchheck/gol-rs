c41df1a8b94f56d8656215ceffacbb6d2a04aff5
Henrique Kirch Heck <86362827+henriquekirchheck@users.noreply.github.com>
2023-09-08 15:05:33 -0300

    Reorganized lib

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use life_rs::{algorithms::gol::GameOfLife, lifealgo::LifeAlgo};
 
 fn main() {
-    let mut gol = GameOfLife::new(318, 135);
+    let mut gol = GameOfLife::new(50, 50);
     gol.set_state_fn(|_| rand::random());
     loop {
-        println!("{gol:?}");
+        println!("{gol}");
         gol.step();
     }
 }
